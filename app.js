43ea3e7c15c5b8c77ebcfd690b5dbefaebf18f18
VijaySenthil1509 <vijaysenthil1509@gmail.com>
2023-06-26 17:04:57 +0530

    vanakam

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1>namaste</h1>;
-
-const Fun = () => (
-  <>
-    <h1>came from function</h1>
-  </>
+const Body = () => (
+  <div className="body-container">
+    <div>Search bar</div>
+    <div className="res-container"></div>
+  </div>
 );
 
-const Fun2 = () => (
-  <>
-    <h2>h2 da idhu</h2>
-    <Fun></Fun>
-  </>
+const Header = () => (
+  <div className="header">
+    <img
+      className="logo-image"
+      alt="logo-image"
+      src="https://media.istockphoto.com/id/1218437637/vector/food-point-logo-design.jpg?s=1024x1024&w=is&k=20&c=1_5v1kzop2K2sQoVRVtySRzIWH1v0qi2HQNQEc5YEno="
+    ></img>
+    <ul>
+      <li>About us</li>
+      <li>Contact us</li>
+      <li>cart</li>
+    </ul>
+  </div>
+);
+
+const Applayout = () => (
+  <div className="app-layout">
+    <Header />
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-const root2 = ReactDOM.createRoot(document.querySelector(".root2"));
-root.render(<Fun />);
-root2.render(<Fun2 />);
+root.render(<Applayout />);
